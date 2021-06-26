94e096f8f9ed6d82f62ee898ee5d02d14b1c80e3
Sarabpreet Singh <56184253+sarabpreets7@users.noreply.github.com>
2021-06-26 22:11:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,10 @@ function addEvent(box,i){
                             <div class="end">
                                 <input placeholder="End time:" type="text">
                             </div>
+
+                            <div class="batch">
+                                <input placeholder="Batch:" type="text">
+                            </div>
                             <button class="submit">Done</div>`
 
         document.querySelector("body").append(modal);
@@ -46,11 +50,13 @@ function addEvent(box,i){
             let topic = document.querySelector(".topic input").value
             let st = document.querySelector(".start input").value
             let end = document.querySelector(".end input").value
+            let batch = document.querySelector(".batch input").value
             box.classList.add("selected")
             boxDat[selectedTeacher][i-1].selected= true;
             boxDat[selectedTeacher][i-1].topic = topic;
                 boxDat[selectedTeacher][i-1].start = st;
                 boxDat[selectedTeacher][i-1].end = end;
+                boxDat[selectedTeacher][i-1].batch = batch;
                 modal.animate({
                     "width":"0vw"
                 },300);
@@ -88,7 +94,9 @@ function loadData(){
             let box = document.createElement("div");
             box.classList.add("row-day");
             box.innerHTML = `<div class="topic"> Topic: ${data[i].topic}</div>
-                             <div class="time">Time: ${data[i].start} - ${data[i].end} </div>`
+                             <div class="time">Time: ${data[i].start} - ${data[i].end} </div>
+                             <div class="Batch">Batch: ${data[i].batch}</div>
+                             `
                              
             box.classList.add("selected");
             box.setAttribute('id',i+1);
@@ -109,6 +117,10 @@ function loadData(){
                             <div class="end">
                                 <input placeholder="End time:" type="text">
                             </div>
+
+                            <div class="batch">
+                                <input placeholder="Batch:" type="text">
+                            </div>
                             <button class="submit">Done</div>`
 
         document.querySelector("body").append(modal);
@@ -120,11 +132,13 @@ function loadData(){
             let topic = document.querySelector(".topic input").value
             let st = document.querySelector(".start input").value
             let end = document.querySelector(".end input").value
+            let batch = document.querySelector(".batch input").value;
             box.classList.add("selected")
             boxDat[selectedTeacher][i].selected= true;
             boxDat[selectedTeacher][i].topic = topic;
                 boxDat[selectedTeacher][i].start = st;
                 boxDat[selectedTeacher][i].end = end;
+                boxDat[selectedTeacher][i].batch = batch;
                 modal.animate({
                     "width":"0vw"
                 },300);
@@ -200,7 +214,7 @@ function myFunction() {
         box.classList.add("row-day");
         box.setAttribute('id',i);
         box.innerText=i;
-        let boxobj = {"selected":false,"topic":"","start":"","end":""}
+        let boxobj = {"selected":false,"topic":"","start":"","end":"","batch":""}
         boxDat[selectedTeacher].push(boxobj);
         $(".calender-days").append(box);
         addEvent(box,i);
@@ -214,6 +228,8 @@ function myFunction() {
 
 
 function popUp(){
+
+
     let modal = document.createElement("div");
         modal.classList.add("pop-up");
         modal.innerHTML=`  <div class="pop-head">Enter Details</div>
@@ -231,6 +247,9 @@ function popUp(){
                             <div class="end">
                                 <input placeholder="End time:" type="text">
                             </div>
+                            <div class="batch">
+                                <input placeholder="Batch:" type="text">
+                            </div>
                             <button class="submit">Done</div>`
 
         document.querySelector("body").append(modal);
@@ -238,11 +257,13 @@ function popUp(){
             "height":"310px",
             "width":"570px"
         },300)
+        
         document.querySelector(".submit").addEventListener("click",function(){
             let topic = document.querySelector(".topic input").value
             let st = document.querySelector(".start input").value
             let end = document.querySelector(".end input").value
             let date = document.querySelector(".date input").value;
+            let batch = document.querySelector(".batch input").value;
             modal.animate({
                 "width":"0vw"
             },300);
@@ -255,7 +276,7 @@ function popUp(){
             boxDat[selectedTeacher][date-1].topic = topic;
                 boxDat[selectedTeacher][date-1].start = st;
                 boxDat[selectedTeacher][date-1].end = end;
-            
+                boxDat[selectedTeacher][date-1].batch = batch;
             loadData();
             
             
@@ -272,4 +293,6 @@ function updateDay(){
         })
     }
 }
+
+
 
